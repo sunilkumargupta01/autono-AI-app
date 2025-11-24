be67b7e4a5ee06f14e32e9688cb3fd3a2a967066
Sunil Kumar <sunilkumar302628@gmail.com>
2025-11-24 17:05:42 +0530

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
       <nav>
         <h1><Link to= '/'>AUTONO</Link></h1>
-        <ul>
+         <GiHamburgerMenu  className='hamburger' onClick={()=> setOpen(!open)}/>
+        <ul className={open? "open" : ""}>
             <li><Link to='/technology'>Technology</Link></li>
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/careers'>Careers</Link></li>
